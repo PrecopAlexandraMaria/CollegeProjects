0db66547201bb6772b125ac0eeeffff52547d83b
Precop Alexandra-Maria <precopalexandramaria@gmail.com>
2026-02-25 12:01:18 +0200

    Delete ArtPortofolioCatalog directory

--- ArtPortofolioCatalog/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/googletest-main/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hp/OneDrive/Desktop/bp2/ArtPortofolioCatalog/googletest-main/googlemock
-# Build directory: C:/Users/hp/OneDrive/Desktop/bp2/ArtPortofolioCatalog/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/googletest-main/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
